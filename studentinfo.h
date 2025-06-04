7ac36213c0c4bbc028c6932e4a8f6782e648caf1
Alexander Alcazar <alcazar_alexander_am@student.smc.edu>
2025-06-04 08:25:44 -0700

    Initial Push

---
+++ studentinfo.h
@@ -0,0 +1,11 @@
+#ifndef STUDENTINFO_H
+#define STUDENTINFO_H
+#include<string>
+
+//Insert your name is student id, letters and numbers only".
+namespace StudentInfo {
+	std::string Name() { return "Alexander Alcazar"; }
+	std::string ID() { return "1567097"; }
+};
+
+#endif
